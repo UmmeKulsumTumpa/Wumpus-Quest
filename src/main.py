c6707fd447c53663cb21ca297a780e3152374144
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-12-28 22:07:16 +0600

    Wrapper file

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from src.WumpusQuestGame import WumpusQuestGame
+
+if __name__ == "__main__":
+    game = WumpusQuestGame(10)
+    game.initialize_game()
+    score, has_gold, is_alive = game.run_game()
+    print(f"Game finished! Score: {score}, Has Gold: {has_gold}, Agent Alive: {is_alive}")
